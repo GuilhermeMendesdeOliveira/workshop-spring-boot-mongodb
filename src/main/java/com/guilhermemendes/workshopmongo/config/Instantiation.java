f8aff0bbe350eec90c80327b51c214db8b5abd07
GuilhermeMendesdeOliveira <mendesoliveiragmo@gmail.com>
2024-01-20 11:22:06 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/guilhermemendes/workshopmongo/config/Instantiation.java
+++ src/main/java/com/guilhermemendes/workshopmongo/config/Instantiation.java
@@ -21,9 +21,9 @@ public class Instantiation implements CommandLineRunner{
 
 		userRepository.deleteAll();
 		
-		User maria = new User(1L, "Maria Brown", "maria@gmail.com");
-		User alex = new User(2L, "Alex Green", "alex@gmail.com");
-		User bob = new User(3L, "Bob Grey", "bob@gmail.com");
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
